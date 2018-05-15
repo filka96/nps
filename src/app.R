a92da0f2648e0c585042cd80f074988e060f629c
Filipp_Semenkov <Filipp_Semenkov@epam.com>
2018-05-15 17:28:35 +0300

    update module search

--- src/app.R
+++ src/app.R
@@ -1,5 +1,20 @@
 library(shiny)
 
-ui <- dget("ui.R")
-server <- dget("server.R")
-shinyApp(ui = ui, server = server)
+PROGRAM_TITLE = 'NPSW'
+UI_FILENAME = "ui.R"
+SERVER_FILENAME = "server.R"
+MODULES_PATH = "modules"
+
+MODULES = list.dirs(path = MODULES_PATH, full.names = FALSE, recursive = FALSE)
+
+shinyApp(
+    do.call(navbarPage, c(title = PROGRAM_TITLE, lapply(1 : length(MODULES), function(i) {
+        tabPanel(MODULES[i], dget(paste(MODULES_PATH, MODULES[i], UI_FILENAME, sep = "/")))
+    }))),
+    function(input, output, session) {
+        lapply(1 : length(MODULES), function(i) {
+            func <- dget(paste(MODULES_PATH, MODULES[i], SERVER_FILENAME, sep="/"))
+            func(input, output, session)
+        })
+    }
+)
--- src/ui.R
+++
@@ -1,7 +0,0 @@
-library(shiny)
-
-TYPES=c('pbinom','qbinom','pnorm','qnorm','prop.test','binom.test')
-
-do.call(navbarPage, c(title = 'NPSW', lapply(1 : length(TYPES), function(i) {
-    tabPanel(TYPES[i], dget(paste("ui/", TYPES[i], ".R", sep = "")))
-})))
