220cd90ae818ab22dc4bf7f9a3625c4e7766ea2a
Paula <pol08586@gmail.com>
2018-05-27 21:52:10 +0300

    update mcnemara test

--- src/modules/Тесты на независимость/Мак-немара/server.R
+++ src/modules/Тесты на независимость/Мак-немара/server.R
@@ -2,5 +2,13 @@ library(shiny)
 library(npsm)
 library(Rfit)
 
+stringToArray <- function(string, splitter = ','){
+    print(string)
+    return (as.numeric(unlist(strsplit(string, splitter))))
+}
+
 function(input, output, session) {
+    output$mcnemarTestResult <- renderPrint({
+        mcnemar.test(stringToArray(input$mcnemarTestX), stringToArray(input$mcnemarTestY), input$mcnemarTestCorrect)
+    })
 }
--- src/modules/Тесты на независимость/Мак-немара/ui.R
+++ src/modules/Тесты на независимость/Мак-немара/ui.R
@@ -2,7 +2,15 @@ library(shiny)
 
 sidebarLayout(
     sidebarPanel(
+        textInput("mcnemarTestX", label = "вектор X.", "80, 100"),
+        textInput("mcnemarTestY", label = "вектор Y.", "10, 110"),
+        checkboxInput("mcnemarTestCorrect", label = "коррекция непрерывности", value = TRUE)
+
     ),
     mainPanel(
+        h2("Функция mcnemar.test()"),
+        h3("Критерий Мак-Немара . (узнать красивое название)"),
+        h5("Синтаксис: mcnemar.test(x, y = NULL, correct = TRUE)."),
+        verbatimTextOutput("mcnemarTestResult")
     )
 )
