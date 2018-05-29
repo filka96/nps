30933834a11c004ce54313e143c6103a875e7085
filka <filka@tutanota.com>
2018-05-29 21:03:19 +0300

    update wilcoxTest

--- src/modules/Тесты на сдвиги/Вилкоксон/server.R
+++ src/modules/Тесты на сдвиги/Вилкоксон/server.R
@@ -2,4 +2,22 @@ library(shiny)
 library(npsm)
 library(Rfit)
 
-function(input, output, session) {}
+stringToArray <- function(string, splitter = ','){
+    return (as.numeric(unlist(strsplit(string, splitter))))
+}
+
+function(input, output, session) {
+    output$wilcoxTestResult <- renderPrint({
+        wilcox.test(
+            x = stringToArray(input$wilcoxTestX),
+            y = NULL,
+            alternative = input$wilcoxTestAlternative,
+            mu = input$wilcoxTestMu,
+            paired = input$wilcoxTestPaired,
+            exact = NULL,
+            correct = input$wilcoxTestCorrect,
+            conf.int = input$wilcoxTestConfInt,
+            conf.level = input$wilcoxTestConfLevel
+        )
+    })
+}
